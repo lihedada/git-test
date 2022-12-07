c571455bc5d051867512740b92571c41498b062d
lihe <lihelihe724@163.com>
2022-12-07 15:43:14 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.git;/*
+@author shkstart
+@Date2022-12-07-15:38
+*/
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
